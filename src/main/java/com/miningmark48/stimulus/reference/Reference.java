e234eb98d3880f68e641e736d8c248cd7368a558
MiningMark48 <miningmark48@gmail.com>
2017-11-11 01:22:57 -0500

    Update

--- src/main/java/com/miningmark48/stimulus/reference/Reference.java
+++ src/main/java/com/miningmark48/stimulus/reference/Reference.java
@@ -5,8 +5,8 @@ public class Reference {
 
     public static final String MOD_ID = "stimulus";
     public static final String MOD_NAME = "Stimulus";
-    public static final String MOD_VERSION = "1.0.0";
-    public static final String VERSION = "1.12-" + MOD_VERSION;
+    public static final String MOD_VERSION = "1.1.0";
+    public static final String VERSION = "1.12.2-" + MOD_VERSION;
     public static final String CLIENT_PROXY_CLASS = "com.miningmark48.stimulus.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.miningmark48.stimulus.proxy.CommonProxy";
     public static final String GUI_FACTORY_CLASS = "com.miningmark48.stimulus.gui.GuiFactory";
